057d67d951fd2ae36c60ce7ec83a955cc3783e03
tporyzala <tomekporyzala@gmail.com>
2023-09-14 12:29:37 -0600

    restructure

--- __init__.py
+++
@@ -1,9 +0,0 @@
-from meteopy.Hourly.HourlyForcast import *
-
-from meteopy.Daily.DailyForcast import *
-
-from meteopy.Options.OptionsForecast import *
-from meteopy.Options.OptionsElevation import *
-from meteopy.Options.OptionsGeocoding import *
-
-from meteopy.MeteoManager import *
---
+++ meteopy/__init__.py
@@ -0,0 +1,9 @@
+from .Hourly.HourlyForcast import *
+
+from .Daily.DailyForcast import *
+
+from .Options.OptionsForecast import *
+from .Options.OptionsElevation import *
+from .Options.OptionsGeocoding import *
+
+from .MeteoManager import *
